d70ed0ddfe64f586347056ce5980f1f9d3f45edc
Sébastien Mosser <sebastien.mosser@gmail.com>
2023-03-05 11:01:31 -0500

    supporting methods as test cases

--- src/main/java/mcunit/TestCase.java
+++ src/main/java/mcunit/TestCase.java
@@ -1,28 +1,80 @@
 package mcunit;
 
-public abstract class TestCase implements Test {
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Optional;
+
+public final class TestCase implements Test {
+
+    private static final String SETUP = "setUp";
+    private static final String TEARDOWN = "tearDown";
+
+    private final Method testMethod;
+    private final Class klass;
+
+    public TestCase(Class klass, Method testMethod) {
+        this.testMethod = testMethod;
+        this.klass = klass;
+    }
 
     @Override
     public final void run(TestReport collector) {
-        TestResult result = new TestResult(this.getClass().getCanonicalName());
+        String title = klass.getCanonicalName() + "::" + testMethod.getName();
+        TestResult result = new TestResult(title);
+        Object context = null;
         try {
-            setUp();
-            test(); // <<-- Design Pattern: Template method
+            context =  klass.getDeclaredConstructor().newInstance();
+            runSetUp(context);
+            runTest(context);
             result.record(STATUS.PASSED);
-        } catch(AssertionError ae) {
-            result.record(STATUS.FAILED);
+        } catch(InvocationTargetException ite) {
+            try {
+                throw ite.getTargetException();
+            } catch (AssertionError ae) {
+                result.record(STATUS.FAILED);
+            } catch(Throwable e) {
+                result.record(STATUS.ERRORED);
+            }
         } catch (Exception e) {
             result.record(STATUS.ERRORED);
         } finally {
-            tearDown();
+            try {
+                runTearDown(context);
+            } catch (Exception e) {
+                result.record(STATUS.ERRORED);
+            }
         }
         collector.collect(result);
     }
 
-    protected void setUp() { }
+    private void runSetUp(Object context)
+            throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        if(getMethodByName(SETUP).isPresent()) {
+            Method setup = getMethodByName(SETUP).get();
+            setup.invoke(context);
+        }
+    }
 
-    protected void tearDown() { }
+    private void runTearDown(Object context)
+            throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        if(getMethodByName(TEARDOWN).isPresent()) {
+            Method setup = getMethodByName(TEARDOWN).get();
+            setup.invoke(context);
+        }
+    }
 
-    protected abstract void test();
+    private void runTest(Object context)
+            throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+            this.testMethod.invoke(context);
 
+    }
+
+    private Optional<Method> getMethodByName(String methodName) {
+        try {
+            Method result = this.klass.getMethod(methodName);
+            return Optional.of(result);
+        } catch(NoSuchMethodException nme) {
+            return Optional.empty();
+        }
+    }
 }
--- src/main/java/tests/IntegerTests.java
+++ src/main/java/tests/IntegerTests.java
@@ -8,49 +8,33 @@ import static mcunit.Assertions.assertEquals;
 public class IntegerTests extends TestSuite {
 
     public IntegerTests() {
-        this.add(new AddTwoIntegers());
-        this.add(new SubtractTwoIntegers());
-        this.add(new ThrowAnException());
-    }
-
-    private static class AddTwoIntegers extends TestCase {
-
-        int x, y;
-
-        @Override
-        protected void setUp() {
-            this.x = 1;
-            this.y = 1;
-        }
-
-        @Override
-        protected void test() {
-            assertEquals(x + y, 2); // should pass
+        try {
+            Class suite = IntegerTests.class;
+            this.add(new TestCase(suite, suite.getMethod("addTwoIntegers")));
+            this.add(new TestCase(suite, suite.getMethod("subtractTwoIntegers")));
+            this.add(new TestCase(suite, suite.getMethod("throwAnException")));
+        } catch (NoSuchMethodException nme) {
+            throw new RuntimeException(nme);
         }
     }
 
-    private static class SubtractTwoIntegers extends TestCase {
+    int x, y;
 
-        int x, y;
-
-        @Override
-        protected void setUp() {
-            this.x = 1;
-            this.y = 1;
-        }
-
-        @Override
-        protected void test() {
-            assertEquals(x - y, 1); // Should fail (assertion error)
-        }
+    public void setUp() {
+        this.x = 1;
+        this.y = 1;
     }
 
-    private static class ThrowAnException extends TestCase {
+    public void addTwoIntegers() {
+        assertEquals(x + y, 2); // should pass
+    }
 
-        @Override
-        protected void test() {
-            throw new RuntimeException("Unexpected Error!");
-        }
+    public void subtractTwoIntegers() {
+        assertEquals(x - y, 1); // Should fail (assertion error)
+    }
+
+    public void throwAnException() {
+        throw new RuntimeException("Unexpected Error!");
     }
 
 }
